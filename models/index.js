d382a04a30d4794c1a408d0c3e52a8f1ac27cf12
robbykurnia <roby.kurniaesaputra@gmail.com>
2019-12-06 17:22:23 +0700

    first commit

---
+++ models/index.js
@@ -0,0 +1,23 @@
+import Sequelize from "sequelize";
+
+const sequelize = new Sequelize("graphql_db", "postgres", "postgres", {
+  dialect: "postgres"
+});
+
+// const Op = Sequelize.Op;
+// console.log(Op);
+
+// Table
+const db = {
+  User: sequelize.import("./user"),
+  Post: sequelize.import("./post"),
+  Comment: sequelize.import("./comment")
+};
+
+Object.keys(db).forEach(models => {
+  if ("associate" in db[models]) db[models].associate(db);
+});
+
+db.sequelize = sequelize;
+
+export default db;
---
+++ models/user.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    username: { type: DataTypes.STRING, unique: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: DataTypes.STRING
+  });
+
+  User.associate = models => {
+    User.hasMany(models.Post, {
+      foreignKey: "creatorPostId"
+    }),
+      User.hasMany(models.Comment, {
+        foreignKey: "creatorCommentId"
+      });
+  };
+
+  return User;
+};
